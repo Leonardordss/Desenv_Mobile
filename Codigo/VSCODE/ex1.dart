49f31fdeb4bf0c100c538e8b7491eb046cf290e0
Leonardordss <lrodrigues.md@gmail.com>
2025-02-03 20:14:46 -0300

    Inserindo mais exemplos

--- Codigo/VSCODE/ex1.dart
+++ Codigo/VSCODE/ex1.dart
@@ -1,19 +1,32 @@
-// importando a biblioteca 
+// Importando a biblioteca 
 
 import 'dart:io';
 
-// função principal do codigo
+// Função principal do codigo
 
 void main(){
-  //declara a variavel tipo inteira
-  //chama a idade
+  // Declarando variavel do tipo inteira
+  // print chama a idade
+
   int idade = 29;
   print("Idade: $idade");
 
   // Declarando variavel do tipo double
+
   double raio = 10.25;
   print("Raio: $raio");
 
+  // Declarando variavel do tipo string
 
+  String nome = "Leonardo";
+  print("Nome $nome");
+  print("Ola $nome! Seja bem vindo! ");
+
+  // Declarando variavel do tipo booleana
+
+  bool st= true;
+  print("Estado da variavel st: $st");
+
+  // 
 
 }
---
+++ Codigo/VSCODE/ex2.dart
@@ -0,0 +1,25 @@
+// Importando a bibliotec dart:io
+
+import 'dart:io';
+
+// Função principal do codigo
+
+void main(){
+
+print("Digite seu nome"); // Exibe a mensagem no terminal
+
+// digite o seu nome
+// null safety
+// so ira converter o dado se a entrada nao for vazia
+
+String nome = stdin.readLineSync()!;
+
+print("Digite sua idade");
+
+String idade = stdin.readLineSync()!;
+
+print("Seu nome é $nome e a sua idade é $idade");
+
+// 
+
+}
