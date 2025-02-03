98b692a4cf3460b04d79e5f5992a1081f7fbb86c
Leonardordss <lrodrigues.md@gmail.com>
2025-02-03 19:38:27 -0300

    Inserindo Variaveis

---
+++ Codigo/VSCODE/ex1.dart
@@ -0,0 +1,19 @@
+// importando a biblioteca 
+
+import 'dart:io';
+
+// função principal do codigo
+
+void main(){
+  //declara a variavel tipo inteira
+  //chama a idade
+  int idade = 29;
+  print("Idade: $idade");
+
+  // Declarando variavel do tipo double
+  double raio = 10.25;
+  print("Raio: $raio");
+
+
+
+}
