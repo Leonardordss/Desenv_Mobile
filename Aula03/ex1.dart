96ec9e1c534c84f83d081636dfb60805d9a34ee3
Leonardordss <lrodrigues.md@gmail.com>
2025-02-10 19:36:51 -0300

    Aula 03 exemplo 01

---
+++ Aula03/ex1.dart
@@ -0,0 +1,18 @@
+
+
+
+
+import 'dart:io';
+
+void main(){
+  int idade;
+  print("Digite sua idade");
+  idade = int.parse(stdin.readLineSync()!);
+
+  if(idade>=18){
+    print("Maior de idade");
+  }
+
+
+
+}
