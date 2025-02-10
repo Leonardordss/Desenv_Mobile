9f0422a9552d881b129dcecb85fb36047e8b6938
Leonardordss <lrodrigues.md@gmail.com>
2025-02-10 19:49:40 -0300

    Aula 03 exemplo 02

---
+++ Aula03/ex2.dart
@@ -0,0 +1,25 @@
+
+import 'dart:io';
+
+void main(){
+  double n1,n2, res;
+  print("Digite o primeiro numero");
+
+  n1 = double.parse(stdin.readLineSync()!);
+
+  print("Digite o segundo numero");
+
+  n2 = double.parse(stdin.readLineSync()!);
+
+  res = (n1+n2)/2;
+  print("Media: $res");
+  if(res>=5){
+    print("Aprovado - media $res");
+  }
+  else{
+    print("Reprovado - media: $res");
+  }
+
+//recebe o numero digitado pelo usuario e converte de string para double
+
+}
