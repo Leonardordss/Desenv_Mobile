12b623fd5a4158cb45207f0b31cf7cb3b9965001
leonardordss <lrodrigues.md@gmail.com>
2025-05-19 21:30:17 -0300

    Aula10 atualizada

--- Aula10/app_aula10/lib/main.dart
+++ Aula10/app_aula10/lib/main.dart
@@ -1,3 +1,7 @@
+
+import 'package:app_aula10/screens/tela2.dart';
+import 'package:app_aula10/screens/tela3.dart';
+import 'package:app_aula10/screens/tela4.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -7,35 +11,51 @@ void main() {
   ));
 }
 
+// cria uma classe do tipo Stateless
 class DashboardScreen extends StatelessWidget {
   const DashboardScreen({super.key});
-
+  // polimorfismo que permite tratar as funções de forma particular
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[200], // Fundo bege (mais terroso)
+      // Drawer propriedade do Scaffold que permite criar um elemento lateral
       drawer: Drawer(
         child: ListView(
+          // adicionando espaçamento com o padding
+          padding: EdgeInsets.zero,
+          // children pois utilizaremos mais de um widget
           children: [
+            // Widget DrawerHeader vamos colocar o titulo do Drawer
             DrawerHeader(
-              decoration: BoxDecoration(
-                color: Colors.green[800], // Verde Escuro
-              ),
-              child: Text('Menu', style: TextStyle(color: Colors.white)),
+                decoration: BoxDecoration(color: Colors.brown),
+                child: Text(
+                  'Menu',
+                  style: TextStyle(color: Colors.white),
+                )),
+            ListTile(
+              title: Text('Opção 1'),
             ),
-            ListTile(title: Text('Opção 1')),
-            ListTile(title: Text('Opção 2')),
+            ListTile(
+              title: Text('Opção 2'),
+            )
           ],
         ),
       ),
+      // criando o appbar
+
       appBar: AppBar(
-        backgroundColor: Colors.green[800], // Verde Escuro
+        // atribuindo cor ao appbar
+        backgroundColor: Colors.red,
         elevation: 5,
-        iconTheme: IconThemeData(color: Colors.amber), // Amarelo
+        iconTheme: IconThemeData(color: Colors.white),
+        // centraliza o titulo do aplicativo
         centerTitle: true,
         title: Row(
           children: [
-            Image.asset('images/senai.png', height: 40),
+            Image.asset(
+              'images/senai.png',
+              height: 40,
+            ),
             Padding(
               padding: EdgeInsets.all(12),
               child: Text(
@@ -43,85 +63,107 @@ class DashboardScreen extends StatelessWidget {
                 style: TextStyle(
                     fontSize: 20,
                     fontWeight: FontWeight.w800,
-                    color: Colors.amber),
+                    color: Colors.white),
               ),
             ),
+
             Spacer(),
+            // permite colocar a foto do usuario logado por exemplo
             CircleAvatar(
-              backgroundColor: Colors.brown[100], // Marrom claro
-              child: Icon(Icons.person, color: Colors.green[800]), // Verde Escuro
-            )
+              backgroundColor: Colors.brown[100],
+              child: Icon(
+                Icons.person,
+                color: Colors.brown,
+              ),
+            ),
           ],
         ),
       ),
+      // corpo do Scaffold
       body: Column(
         children: [
           Padding(
-            padding: EdgeInsets.all(12),
+            padding: const EdgeInsets.all(12.0),
             child: TextField(
               decoration: InputDecoration(
-                hintText: 'Buscar ...',
-                prefixIcon: Icon(Icons.search),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(30),
-                ),
-              ),
+                  hintText: 'Buscar ...',
+                  prefixIcon: Icon(Icons.search),
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30))),
             ),
           ),
-          SizedBox(height: 10),
-          _DashboardButton(
+          SizedBox(
+            height: 20,
+          ),
 
+          // Cria botoes em Card
+          _DashboardButton(
               icon: Icons.analytics_outlined,
               label: 'Monitoramento',
-              onTap: () {}),
-          SizedBox(height: 10),
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => ColetaDadosScreen()));
+              }),
+
+          SizedBox(
+            height: 10,
+          ),
           _DashboardButton(
               icon: Icons.settings_remote_outlined,
               label: 'Sistema de controle',
-              onTap: () {}),
-          SizedBox(height: 10),
+              onTap: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => Telaacionamento()));
+              }),
+
+          SizedBox(
+            height: 10,
+          ),
           _DashboardButton(
-              icon: Icons.smart_toy_outlined, label: 'Chatbot', onTap: () {}),
+              icon: Icons.smart_toy_outlined, label: 'Chatbot', onTap: () {
+                Navigator.push(context, MaterialPageRoute(
+                  builder: (context)=>ChatScreen()));
+              }),
         ],
       ),
+      // cria widget Botton Navigator
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: 0,
-        selectedItemColor: Colors.amber, // Amarelo
-        backgroundColor: Colors.green[800], // Verde Escuro
+        selectedItemColor: Colors.white,
+        backgroundColor: Colors.red[900],
         items: [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: '',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.settings),
-            label: '',
-          ),
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: ''),
+          BottomNavigationBarItem(icon: Icon(Icons.settings), label: ''),
         ],
       ),
     );
   }
 }
 
+// Cria uma classe chamada DashboardButton
+
 class _DashboardButton extends StatelessWidget {
+  // Criando atributos para a classe
+
   final IconData icon;
   final String label;
   final VoidCallback onTap;
+
+  // Criando construtor com passagem de parametros obrigatorios
 
   const _DashboardButton(
       {required this.icon, required this.label, required this.onTap});
 
   @override
   Widget build(BuildContext context) {
+    //  novo widget que permite tocar na tela
     return InkWell(
-    
       onTap: onTap,
       child: Card(
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(16),
-        ),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
         elevation: 5,
-        color: Colors.green[100],
         child: Padding(
           padding: EdgeInsets.symmetric(vertical: 50, horizontal: 12),
           child: Row(
@@ -131,11 +173,13 @@ class _DashboardButton extends StatelessWidget {
                 color: Colors.brown,
                 size: 40,
               ),
-              SizedBox(width: 16),
+              SizedBox(
+                width: 16,
+              ),
               Text(
                 label,
-                style: TextStyle(fontSize: 18, color: Colors.brown), // Amarelo
-              ),
+                style: TextStyle(fontSize: 18, color: Colors.brown),
+              )
             ],
           ),
         ),
